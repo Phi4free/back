346b13b5e1826175bffcb1c7337d4df30fe9b316
BeatrizNataly <beatriznatyce@gmail.com>
2023-05-18 19:14:34 -0300

    Tradução parcial das mensagens do servidor  concluída

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,28 @@
-const { dbCreateUser, dbReadUser, dbUpdateUser, dbDeleteUser } = require('./dbController');
-const { isUserPopulated, charLimit, hasValidEmail, hasStrongPassword, runValidationTests } = require('../middlewares/inputValidation');
+const {
+    dbCreateUser,
+    dbReadUser,
+    dbUpdateUser,
+    dbDeleteUser,
+} = require("./dbController");
+const {
+    isUserPopulated,
+    charLimit,
+    hasValidEmail,
+    hasStrongPassword,
+    runValidationTests,
+} = require("../middlewares/inputValidation");
+const Tradutor = require("../tradutor");
 
 module.exports.verPerfilGet = async (request, response) => {
     id = request.query.id;
     try {
         const result = await dbReadUser(id);
-        response.status(result.status).send({ message: result.message, data: result.user });
+        response
+            .status(result.status)
+            .send({ message: result.message, data: result.user });
     } catch (error) {
         console.log(error);
-        response.status(500).send({ message: 'Internal Server Error' });
+        response.status(500).send({ message: Tradutor.t("error") });
     }
 };
 
@@ -19,36 +33,42 @@ module.exports.atualizarPerfilPut = async (request, response, next) => {
         isUserPopulated,
         charLimit,
         hasValidEmail,
-        hasStrongPassword
+        hasStrongPassword,
     ];
-    
+
     const validationResult = runValidationTests(user, tests);
     if (validationResult) {
-        return response.status(validationResult.status).send({ message: validationResult.message });
+        return response
+            .status(validationResult.status)
+            .send({ message: validationResult.message });
     }
 
     try {
         const result = await dbUpdateUser(user);
-        response.status(result.status).send({ message: result.message, data: result.updatedUser });
+        response
+            .status(result.status)
+            .send({ message: result.message, data: result.updatedUser });
     } catch (error) {
         console.log(error);
-        response.status(500).send({ message: 'Internal Server Error' });
+        response.status(500).send({ message: Tradutor.t("error") });
     }
 };
 
 module.exports.criarPerfilPost = async (request, response, next) => {
     const user = request.body;
-  
+
     const tests = [
-      isUserPopulated,
-      charLimit,
-      hasValidEmail,
-      hasStrongPassword
+        isUserPopulated,
+        charLimit,
+        hasValidEmail,
+        hasStrongPassword,
     ];
-  
+
     const validationResult = runValidationTests(user, tests);
     if (validationResult) {
-      return response.status(validationResult.status).send({ message: validationResult.message });
+        return response
+            .status(validationResult.status)
+            .send({ message: validationResult.message });
     }
 
     try {
@@ -61,7 +81,7 @@ module.exports.criarPerfilPost = async (request, response, next) => {
         }
     } catch (error) {
         console.log(error);
-        response.status(500).send({ message: 'Internal Server Error' });
+        response.status(500).send({ message: Tradutor.t("error") });
     }
 };
 
@@ -69,10 +89,11 @@ module.exports.deletarPerfilDelete = async (request, response, next) => {
     id = request.params.id;
     try {
         const result = await dbDeleteUser(id);
-        response.status(result.status).send({ message: result.message, data: result.deletedCount });
+        response
+            .status(result.status)
+            .send({ message: result.message, data: result.deletedCount });
     } catch (error) {
         console.log(error);
-        response.status(500).send({ message: 'Internal Server Error' });
+        response.status(500).send({ message: Tradutor.t("error") });
     }
-      
 };
--- tradutor.js
+++ tradutor.js
@@ -8,12 +8,28 @@ const Tradutor = {
     resources : {
         en: {
             translation: {
-                invalidEmail: "Invalid Email",
+                error: 'Internal Server Error',
+                invalidEmail: 'Invalid Email',
+                article403: 'Your current account cannot create articles. Contact us to validate your profile as a content creator!',
+                createArticle404: 'Error creating article',
+                readArticle404: 'Article not found',
+                listArticle404: 'No articles were found',
+                createUser404: 'Error creating user, please try again later',
+                emailInUse400: 'This e-mail is already in use',
+                readUser404: 'User not found',
             }
         },
         "pt-BR": {
             translation:{
-                invalidEmail: "Email inválido",
+                error: 'Erro interno do servidor',
+                invalidEmail: 'Email inválido',
+                article403: 'Sua conta atual não pode criar artigos. Entre em contato conosco para validar seu perfil como criador de conteúdo!',
+                createArticle404: 'Erro ao criar artigo',
+                readArticle404: 'Artigo não encontrado',
+                listArticle404: 'Nenhum artigo encontrado',
+                createUser404: 'Erro ao criar usuário, por favor tente novamente mais tarde',
+                emailInUse400: 'Este e-mail já está sendo usado',
+                readUser404: 'Usuário não encontrado',
             }
         },
     },
