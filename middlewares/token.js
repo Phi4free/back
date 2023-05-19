141b5aec9989fec6d58ce0882967c0a86f770b65
Gabriel Parreira <gabriel.s.parreira@gmail.com>
2023-05-19 17:01:21 -0300

    desacoplamento da verifyAuthor e verifyUser da verifyJWT

--- middlewares/token.js
+++ middlewares/token.js
@@ -27,35 +27,41 @@ const verifyJWT = async (req, res, next) => {
 }
 
 const verifyAuthor = async (req, res, next) => {
-  await verifyJWT(req, res, async () => {
-    let articleId = (req.body._id || req.params.id);
-    let bdReq = await dbReadArticle(articleId);
-    if (bdReq.status != 404) {
-      if (req.user._id.toString() === bdReq.article.autorId.toString()) {
-        next();
+    try {
+      let articleId = (req.body._id || req.params.id);
+      let bdReq = await dbReadArticle(articleId);
+      if (bdReq.status != 404) {
+        if (req.user._id.toString() === bdReq.article.autorId.toString()) {
+          next();
+        } else {
+          return res.status(403).send({ auth: false, message: 'You cannot access this article', status: 403 });
+        }
       } else {
-        return res.status(403).json({ auth: false, message: 'You cannot access this article', status: 403 });
+        return res.send(bdReq);
       }
-    } else {
-      return res.send(bdReq);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({message: "Internal Server error", status: 500});
     }
-  });
 };
 
 const verifyUser = async (req, res, next) => {
-  await verifyJWT(req, res, async () => {
+  try {
     let reqId = (req.body._id || req.params.id);
     let bdReq = await dbReadUser(reqId);
     if (bdReq.status != 404) {
       if (req.user._id.toString() === bdReq.user._id.toString()) {
         next();
       } else {
-        return res.status(403).json({ auth: false, message: 'User mismatch', status: 403 });
+        return res.status(403).send({ auth: false, message: 'User mismatch', status: 403 });
       }
     } else {
       return res.status(bdReq.status).send({ message: bdReq.message });
     }
-  });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({message: "Internal Server error", status: 500});
+  }
 };
 
 module.exports = { verifyJWT, verifyAuthor, verifyUser };
