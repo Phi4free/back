220dbe72b299165e11d569d710a9d793ea29d870
Gabriel Parreira <gabriel.s.parreira@gmail.com>
2023-05-18 22:52:47 -0300

    primeira access control list configurada

---
+++ middlewares/permissions.js
@@ -0,0 +1,6 @@
+const accessControlList = {
+    'Student': ['lerArtigo', 'listaArtigo', 'verPerfil', 'atualizarPerfil'],
+    'Teacher': ['lerArtigo', 'listaArtigo', 'criarArtigo', 'atualizarArtigo', 'excluirArtigo', 'verPerfil', 'atualizarPerfil'],
+    'Curator': ['lerArtigo', 'listaArtigo', 'criarArtigo', 'atualizarArtigo', 'excluirArtigo', 'verPerfil', 'atualizarPerfil'],
+    'Administrator': ['lerArtigo', 'listaArtigo', 'criarArtigo', 'atualizarArtigo', 'excluirArtigo', 'verPerfil', 'atualizarPerfil', 'excluirPerfil']
+}
