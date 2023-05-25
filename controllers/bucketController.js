c846c4907ff6b059e0e8a2bf85c6df8472da903b
Gabriel Parreira <gabriel.s.parreira@gmail.com>
2023-05-25 12:23:59 -0300

    função uploadToCloudinary() adicionada

--- controllers/bucketController.js
+++ controllers/bucketController.js
@@ -1,4 +1,6 @@
+const { request, response } = require('express');
 const multer = require('multer');
+const Tradutor = require('../tradutor');
 const cloudinary = require('cloudinary').v2
 
 module.exports.saveToMemory = (request, response) => {
@@ -6,4 +8,18 @@ module.exports.saveToMemory = (request, response) => {
     const upload = multer({ storage: storage });
     upload.single('image');
     next();
+}
+
+module.exports.uploadToCloudinary = async (request, response) => {
+    try {
+        const fileData = request.file.buffer;
+        const uploadResult = await cloudinary.uploader.upload(fileData);
+        request.cloudinary = uploadResult;
+        request.file.buffer = null;
+        next();
+    } catch (error) {
+        request.file.buffer = null;
+        console.log(error);
+        response.status(500).send({message: Tradutor.t('error'), status: 500});
+    }
 }
