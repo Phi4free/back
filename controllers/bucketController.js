b81bc66748d3c776a71e1588123bbb793ee76b26
Gabriel Parreira <gabriel.s.parreira@gmail.com>
2023-05-25 12:40:09 -0300

    configuração do cloudinary

--- controllers/bucketController.js
+++ controllers/bucketController.js
@@ -2,6 +2,11 @@ const { request, response } = require('express');
 const multer = require('multer');
 const Tradutor = require('../tradutor');
 const cloudinary = require('cloudinary').v2
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_APIKEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
 
 module.exports.saveToMemory = (request, response) => {
     const storage = multer.memoryStorage();
